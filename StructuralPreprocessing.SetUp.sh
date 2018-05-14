f2fa1299492faa155e8814a4f7ec4aba55699bca
Timothy B. Brown <tbbrown@wustl.edu>
2018-05-14 14:55:11 -0500

    testing FreeSurfer v6 on HCP-YA subjects

--- StructuralPreprocessing.SetUp.sh
+++ StructuralPreprocessing.SetUp.sh
@@ -3,22 +3,22 @@ echo "----- Begin: StructuralPreprocessing.SetUp.sh -----"
 echo ""
 
 echo "Setting up FSL"
-export FSLDIR=/export/HCP/fsl-5.0.10-custom-20171103
+export FSLDIR=/export/HCP/fsl-5.0.11
 source ${FSLDIR}/etc/fslconf/fsl.sh
 echo "Set up to use FSL at ${FSLDIR}"
 echo ""
 
 echo "Setting up FreeSurfer"
 export FSL_DIR="${FSLDIR}"
-#export FREESURFER_HOME=/export/HCP/freesurfer-6.0-custom-20180216
-export FREESURFER_HOME=/act/freesurfer-5.3.0-HCP
+#export FREESURFER_HOME=/act/freesurfer-5.3.0-HCP
+export FREESURFER_HOME=/export/HCP/freesurfer-6.0-custom-20180216
 source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
 
-# modify LD_LIBRARY_PATH to allow access to libnetcdf.so.6 and libhdf5_hl.so.6
-# the mris_make_surfaces binary in the v5.3.0-HCP version of FreeSurfer needs these
-export LD_LIBRARY_PATH=/export/HCP/lib:${LD_LIBRARY_PATH} 
-echo "Set up to use FreeSurfer at ${FREESURFER_HOME}"
-echo ""
+# # modify LD_LIBRARY_PATH to allow access to libnetcdf.so.6 and libhdf5_hl.so.6
+# # the mris_make_surfaces binary in the v5.3.0-HCP version of FreeSurfer needs these
+# export LD_LIBRARY_PATH=/export/HCP/lib:${LD_LIBRARY_PATH} 
+# echo "Set up to use FreeSurfer at ${FREESURFER_HOME}"
+# echo ""
 
 echo "Setting up Python"
 export EPD_PYTHON_HOME=/export/HCP/epd-7.3.2
@@ -28,12 +28,14 @@ echo ""
 
 echo "Setting up Workbench (a.k.a. CARET7)"
 #export CARET7DIR=/export/HCP/workbench-v1.2.3/bin_rh_linux64
-export CARET7DIR=/export/HCP/workbench-dev-20180328/bin_rh_linux64
+#export CARET7DIR=/export/HCP/workbench-dev-20180328/bin_rh_linux64
+export CARET7DIR=/export/HCP/workbench-v1.3.0/bin_rh_linux64
 echo "Set up to use Workbench at ${CARET7DIR}"
 echo ""
 
 echo "Setting up HCP Pipelines"
-export HCPPIPEDIR=${HOME}/pipeline_tools/Pipelines_dev
+#export HCPPIPEDIR=${HOME}/pipeline_tools/Pipelines_dev
+export HCPPIPEDIR=/export/HCP/Pipelines_hotfix_v3.25.0
 export HCPPIPEDIR_Config=${HCPPIPEDIR}/global/config
 export HCPPIPEDIR_Global=${HCPPIPEDIR}/global/scripts
 export HCPPIPEDIR_Templates=${HCPPIPEDIR}/global/templates
